cc7c18c4d756c9772488bd1490d5bacc06f32b85
kevin-pratama <kevinpratama18@gmail.com>
2021-09-24 16:56:22 +0700

    Update

--- components/sidebar.js
+++ components/sidebar.js
@@ -25,7 +25,7 @@ const Sidebar = () => {
 
   return (
     <div className="min-h-screen flex flex-row float-left">
-      <div className="flex flex-col w-60 bg-white overflow-hidden bg-gray-50">
+      <div className="flex flex-col w-60 bg-white overflow-hidden bg-green-50">
         <div className="flex items-center justify-center my-12">
         <Image
                 src="/logo.png"
@@ -36,7 +36,7 @@ const Sidebar = () => {
         </div>
         <ul className="flex flex-col py-4">
           <li>
-            <Link href="/index">
+            <Link href="../../admin">
               <a className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-glintsBlack hover:text-glintsRed">
                 <span className="inline-flex items-center justify-center h-12 w-12"></span>
                 <p className="text-sm font-bold">Home</p>
@@ -44,13 +44,24 @@ const Sidebar = () => {
             </Link>
           </li>
           <li>
-            <Link href="./editAdmin">
+            <Link href="../../admin/user">
             <a
               href="#"
               className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-glintsBlack hover:text-glintsRed"
             >
               <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"></span>
               <p className="text-sm font-bold">Worker</p>
+            </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="../../admin/office">
+            <a
+              href="#"
+              className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-glintsBlack hover:text-glintsRed"
+            >
+              <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"></span>
+              <p className="text-sm font-bold">Office</p>
             </a>
             </Link>
           </li>
